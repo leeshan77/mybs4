16d091dd7d2b8500097366528bdf74fd95a840df
lee seung han <leeshan77@naver.com>
2021-03-13 14:43:14 +0900

    selenium

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -3,7 +3,20 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import pyperclip
+import time
+
 bp = Blueprint('main', __name__, url_prefix='/')
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('headless')
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("lang=ko_KR")
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36")
 
 @bp.route('/')
 def index():
@@ -16,8 +29,6 @@ def clock():
 
 @bp.route('/stock', methods=['POST'])
 def stock():
-    # company_codes = ["005930", "000660", "005380"]
-
     data = request.get_json()
     code1 = data['code1']
     code2 = data['code2']
@@ -28,10 +39,12 @@ def stock():
     company_codes.append(code2)
     company_codes.append(code3)
 
-    prices = []
-    for item in company_codes:
-        now_price = get_price(item)
-        prices.append(now_price)
+    prices = selenium_price(company_codes)
+
+    #prices = []
+    #for item in company_codes:
+    #    now_price = selenium_price(item)
+    #    prices.append(now_price)
 
     sise = {
         'code1': prices[0], 'code2': prices[1], 'code3': prices[2]
@@ -39,7 +52,7 @@ def stock():
 
     return jsonify(result2= "ok", result3= sise, now= datetime.today())
 
-def get_price(company_code):
+def bs4_price(company_code):
     bs_obj = get_bsoup(company_code)
     no_today = bs_obj.find("p", {"class": "no_today"})
     blind = no_today.find("span", {"class": "blind"})
@@ -58,5 +71,25 @@ def get_bsoup(company_code):
         return bs_obj
     else:
         print(result.status_code)
+
+def selenium_price(company_codes):
+    driver = webdriver.Chrome('/selenium/chromedriver', chrome_options=chrome_options)
+    driver.implicitly_wait(10)
+
+    prices = []
+    for code in company_codes:
+        url = 'https://m.kbsec.com/go.able?linkcd=m04010000&flag=0&JmGb=K&stockcode=' + code
+        driver.get(url)
+        driver.implicitly_wait(10)
+
+        select = '#container > form > div.stockInfoBox > div:nth-child(1) > div.cellL.stockToday.stockUp > strong'
+        time.sleep(1)
+        selected = driver.find_element_by_css_selector(select)
+
+        now_price = selected.text
+        prices.append(now_price)
+
+    return prices
+
 
 
